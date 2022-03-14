b478c6e9a194e17738032a6f656b3ff268105812
Italo Maciel <italo.maciel.118@ufrn.edu.br>
2022-03-13 22:56:18 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,26 @@
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Pagination from "components/Pagination";
 import MovieCard from "components/MovieCard";
 import {BASE_URL} from "utils/requests";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-    //Forma Errada
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
         .then(response => {
-            console.log(response.data);
+            const data = response.data as MoviePage;
+            console.log(data);
+            setPageNumber(data.number);
         });
+    }, []);
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
